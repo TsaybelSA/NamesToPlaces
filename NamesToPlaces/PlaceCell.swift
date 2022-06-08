dfa73c2c8e35fb2ba96051d3e3f70f0549c84668
TsaybelSA <tsaybelsa@gmail.com>
2022-06-08 16:05:17 +0600

    Changed main view to be UICollectionView. On main stroryboard created CollectionView with cells with identifier "Place". At main view controller class added navigation item with bounded function to add new photo.

---
+++ NamesToPlaces/PlaceCell.swift
@@ -0,0 +1,15 @@
+//
+//  PlaceCell.swift
+//  NamesToPlaces
+//
+//  Created by Сергей Цайбель on 08.06.2022.
+//
+
+import UIKit
+
+class PlaceCell: UICollectionViewCell {
+	@IBOutlet var imageView: UIImageView!
+	@IBOutlet var name: UILabel!
+	
+
+}
--- NamesToPlaces/ViewController.swift
+++ NamesToPlaces/ViewController.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPlace))
+		
 	}
-
-
+	
+	@objc func addNewPlace() {
+		let picker = UIImagePickerController()
+		picker.delegate = self
+		picker.allowsEditing = true
+		present(picker, animated: true)
+	}
+	
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+		guard let image = info[.editedImage] as? UIImage else { return }
+		
+		let imageName = UUID().uuidString
+		let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+		
+		if let jpeg = image.jpegData(compressionQuality: 0.7) {
+			try? jpeg.write(to: imagePath)
+		}
+		dismiss(animated: true)
+	}
+	
+	func getDocumentsDirectory() -> URL {
+		let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+		return path[0]
+	}
+	
+	override func numberOfSections(in collectionView: UICollectionView) -> Int {
+		return 10
+	}
+	
+	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Place", for: indexPath)
+		return cell
+	}
 }
 
